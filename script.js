9361d185eb9a638efba68c609ec55234cbbbf5ee
Burlacu Ioan Sabin <118127389+S4BIN@users.noreply.github.com>
2023-03-27 21:53:39 +0300

    index.html and script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Calculator {
 
     clear() {
         this.currentOperand = ''
-        this.previousOPerand = ''
+        this.previousOperand = ''
         this.operation = undefined
     }
 
@@ -36,7 +36,7 @@ class Calculator {
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete')
+const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
