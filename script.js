28a0f42939bf1311475f95658796bcd414f2cdb1
Burlacu Ioan Sabin <118127389+S4BIN@users.noreply.github.com>
2023-04-12 20:14:43 +0300

    index.html and script.js and style.css

--- script.js
+++ script.js
@@ -1,58 +1,115 @@
-class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.currentOperand = ''
-        this.operation = undefined
+const numbers = document.querySelectorAll(".numbers");
+const result = document.querySelector(".result span");
+const signs = document.querySelectorAll(".sign");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const negative = document.querySelector(".negative");
+const percent = document.querySelector(".percent");
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let sign = "";
+let resultValue = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", (e) => {
+    let atr = e.target.getAttribute("value");
+    if (isFirstValue === false) {
+      getFirstValue(atr);
     }
-
-    clear() {
-        this.currentOperand = ''
-        this.previousOperand = ''
-        this.operation = undefined
+    if (isSecondValue == false) {
+      getSecondValue(atr);
     }
-
-    delete() {
-
-    }
-
-    appendNumber(number) {
-        if (number === '.' && this.currentOperand.includes('.')) return
-        this.currentOperand = this.currentOperand.toString() + number.toString()
-    }
-
-    chooseOperation(operation) {
-        if (this.currentOperand === "") return;
-        if (this.previousOperand !== "") {
-          this.compute();
-        }
-        this.operation = operation;
-        this.previousOperand = this.currentOperand;
-        this.currentOperand = "";
-      }
-    }
-
-    compute() {}
-
-    updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
-    }
+  });
 }
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+function getFirstValue(el) {
+  result.innerHTML = "";
+  firstValue += el;
+  result.innerHTML = firstValue;
+  firstValue = +firstValue;
+}
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+function getSecondValue(el) {
+  if (firstValue != "" && sign != "") {
+    secondValue += el;
+    result.innerHTML = secondValue;
+    secondValue = +secondValue;
+  }
+}
 
-numberButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      calculator.appendNumber(button.innerText);
-      calculator.updateDisplay();
+function getSign() {
+  for (let i = 0; i < signs.length; i++) {
+    signs[i].addEventListener("click", (e) => {
+      sign = e.target.getAttribute("value");
+      isFirstValue = true;
     });
-  });
+  }
+}
+getSign();
+
+equals.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (sign === "+") {
+    resultValue = firstValue + secondValue;
+  } else if (sign === "-") {
+    resultValue = firstValue - secondValue;
+  } else if (sign === "x") {
+    resultValue = firstValue * secondValue;
+  } else if (sign === "/") {
+    resultValue = firstValue / secondValue;
+  }
+  result.innerHTML = resultValue;
+  firstValue = resultValue;
+  secondValue = "";
+
+  checkResultLenght();
+});
+
+function checkResultLenght() {
+  resultValue = JSON.stringify(resultValue);
+
+  if (resultValue.length >= 8) {
+    resultValue = JSON.parse(resultValue);
+    result.innerHTML = resultValue.toFixed(5);
+  }
+}
+
+negative.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (firstValue != "") {
+    resultValue = -firstValue;
+    firstValue = resultValue;
+  }
+
+  if (firstValue != "" && secondValue != "" && sign != "")
+    resultValue = -resultValue;
+
+  result.innerHTML = resultValue;
+});
+
+percent.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (firstValue != "") {
+    resultValue = firstValue / 100;
+    firstValue = resultValue;
+  }
+
+  if (firstValue != "" && secondValue != "" && sign != "")
+    resultValue = resultValue / 100;
+
+  result.innerHTML = resultValue;
+});
+
+clear.addEventListener("click", () => {
+  result.innerHTML = 0;
+
+  firstValue = "";
+  isFirstValue = false;
+  secondValue = "";
+  isSecondValue = false;
+  sign = "";
+  resultValue = 0;
+});
